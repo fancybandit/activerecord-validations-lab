c8083e78d752d71c37b8b29ce336ba110cb2ac02
Jacob Klimenko <droidedjake@gmail.com>
2021-03-27 12:59:39 -0400

    commit tree 23ba2a7d1f5b5826fdb4e1615fef669a333a0643

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbaity
+
+    private
+
+    def title_is_clickbaity
+        baits = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        if title && baits.none? {|bait| title.include?(bait)}
+            errors.add(:title, "must conatin \"Won't Believe\", \"Secret\", \"Top [number]\", or \"Guess\"")
+        end
+    end
 end
+
